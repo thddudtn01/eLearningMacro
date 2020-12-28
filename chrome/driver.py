178a2443d3613c8b0d544f40b095ee2e825a5cc7
thddudtn01 <54704254+thddudtn01@users.noreply.github.com>
2020-12-28 19:16:01 +0900

    [C] entering study class

--- chrome/driver.py
+++ chrome/driver.py
@@ -1,14 +1,13 @@
-waitForLoginTime = 30
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import time
 
-class driver:
+class chrome:
     def __init__(self):
         self.driver = webdriver.Chrome(executable_path='./chromedriver.exe')
-        return self
     
     def visitAndLogin(self, url):
         self.driver.get(url)
@@ -20,20 +19,52 @@ class driver:
         loginBtn = self.driver.find_element_by_xpath(
             '//*[@id="header"]/div/div[1]/div[2]/ul/li[1]/a')
         self.driver.execute_script("arguments[0].click();", loginBtn)
-        return self
 
-    def waitForLogin(self):
+    def switchToLoginWindow(self):
+        self.driver.switch_to.window(self.driver.window_handles[1])
+
+    def waitForLogin(self, waitForLoginTime):
         WebDriverWait(self.driver, waitForLoginTime).until(
             EC.presence_of_element_located((By.CLASS_NAME, 'myClassBox'))
             )
-        return self
 
     def switchToLoginWindow(self):
-        driver.switch_to.window(driver.window_handles[1])
-        return self
+        self.driver.switch_to.window(self.driver.window_handles[1])
 
-    def visitClass(self, classNum):
+    def visitClassFirst(self):
+        self.driver.switch_to.window(self.driver.window_handles[0])
         classBtn = self.driver.find_element_by_xpath(
-            '//*[@id="cntnr1"]/div/div[2]/div[1]/ul/li[' + str(classNum) + ']')
+            '//*[@id="cntnr1"]/div/div[2]/div[1]/ul/li[1]')
         self.driver.execute_script("arguments[0].click();", classBtn)
-        return self
+        self.driver.switch_to.window(self.driver.window_handles[1])
+
+    #TODO
+    def visitClassLater(self):
+        self.driver.switch_to.window(self.driver.window_handles[0])
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+
+    #TODO
+    def studyRoutine(self, i, j):
+        continue
+
+    def classRoutine(self):
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        ol = soup.select_one('#lo_tag')
+        liCnt = 2
+        for li in ol.select('li.oneDepth'):
+            liCnt += 1
+
+            IntroBtn = self.driver.find_element_by_xpath(
+                '//*[@id="lo_tag"]/li[' + str(liCnt) + ']/div/div[2]/a')
+            self.driver.execute_script("arguments[0].click();", IntroBtn)
+
+            for liCnt2 in range(len(li.select_one('ol').select('li'))):
+                print("li : " + str(liCnt)+ " li : " + str(liCnt2))
+                studyBtn = self.driver.find_element_by_xpath(
+                    '//*[@id="lo_tag"]/li['+str(liCnt)+']/ol/li['+str(liCnt2+1)+']/div/div[2]/a')
+                self.driver.execute_script("arguments[0].click();", studyBtn)
+                time.sleep(3)
+
+                studyRoutine()
+
+        return soup
